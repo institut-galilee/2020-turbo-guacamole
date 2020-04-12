21c8e70a49393b40defcaf9ba8ce6a19595deb48
Hariss123 <59176975+Hariss123@users.noreply.github.com>
2020-04-12 16:37:23 +0200

    Update application_web.ino

--- src/application_web.ino
+++ src/application_web.ino
@@ -1,5 +1,6 @@
 #include <WiFi.h>
 #include <Servo.h>
+#include "pitches.h" // pour utiliser la fonction tone
 
 // Remplacer "" par son réseau, nom et mot de passe de son réseau
 const char* ssid = "";
@@ -11,12 +12,14 @@ int led_box2 = 26;
 int servoPin = 13;
 int led_presence1 = 16;
 int led_presence2 = 17;
+const byte buzzer = 18; 
 
 // On initialise les états des composants a off au début
 String etat_led1 = "off";
 String etat_led2 = "off";
 String etat_porte1 = "close";
 String etat_porte2 = "close";
+String etat_alarme = "eteint";
 
 // définition des broches utilisées 
 int trig_presence = 12; 
@@ -49,6 +52,7 @@ void setup() {
   pinMode(trig_presence, OUTPUT); 
   pinMode(led_presence1, OUTPUT);
   pinMode(led_presence2, OUTPUT);
+  pinMode(buzzer, OUTPUT); 
   
   // on met les leds en éteinte
   digitalWrite(led_box1, LOW);
@@ -83,167 +87,190 @@ void loop() {
   digitalWrite(trig_presence, LOW); 
   lecture_echo = pulseIn(echo, HIGH); 
   cm = lecture_echo / 58; // lecture_echo*340/(2*10000)
-  
-  // Si on détecte une présence on allume les leds
-  if(cm > 0 && cm < 40){
-    digitalWrite(led_presence1, HIGH);
-    digitalWrite(led_presence2, HIGH);
-    delay(500);
-  } else {
-    digitalWrite(led_presence1, LOW);
-    digitalWrite(led_presence2, LOW);
-    delay(100);
-  }
-  
+
   Serial.print("Distance en cm : "); 
   Serial.println(cm); 
   
   // On vérifie si un client se connecte
   WiFiClient client = server.available();
-  
-  if (client) {
-    Serial.println("Nouveau client");
-    String currentLine = "";
-    while (client.connected()) {            
-      if (client.available()) { // On vérifie si le client a envoyé une requete            
-        char c = client.read();             
-        Serial.write(c);                    
-        header += c;
-         if (c == '\n') {   
-          // Fin de la requette HTTP du client
-          if (currentLine.length() == 0) {
-            // HTTP headers 
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // Allumer et eteindre leds
-            if (header.indexOf("GET /1/on") >= 0) {
-              Serial.println("led 1 on");
-              etat_led1 = "on";
-              digitalWrite(led_box1, HIGH);
-            } else if (header.indexOf("GET /1/off") >= 0) {
-              Serial.println("led 1 off");
-              etat_led1 = "off";
-              digitalWrite(led_box1, LOW);
-            } else if (header.indexOf("GET /2/on") >= 0) {
-              Serial.println("led 2 on");
-              etat_led2 = "on";
-              digitalWrite(led_box2, HIGH);
-            } else if (header.indexOf("GET /2/off") >= 0) {
-              Serial.println("led 2 off");
-              etat_led2 = "off";
-              digitalWrite(led_box2, LOW);
-            }
-            
-            // Ouvrir et fermer porte
-            if (header.indexOf("GET /p1/open") >= 0) {
-              Serial.println("porte 1 open");
-              etat_porte1 = "open";
-              for (pos = 0; pos <= 90; pos += 1) { // on tourne jusqu'a 90 degrés 
-                servo.write(pos);
-                delay(10);
+  if(cm > 0 && cm < 40){   // Si on détecte une présence et que la personne se connecte on allume les leds
+    if (client) {
+      Serial.println("Nouveau client");
+      
+      // On allume les led pour que la personne ait de la visibilité
+      digitalWrite(led_presence1, HIGH);
+      digitalWrite(led_presence2, HIGH);
+      delay(500);
+      
+      String currentLine = "";
+      while (client.connected()) {            
+        if (client.available()) { // On vérifie si le client a envoyé une requete            
+          char c = client.read();             
+          Serial.write(c);                    
+          header += c;
+           if (c == '\n') {   
+            // Fin de la requette HTTP du client
+            if (currentLine.length() == 0) {
+              // HTTP headers 
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-type:text/html");
+              client.println("Connection: close");
+              client.println();
+              
+              // Allumer et eteindre leds
+              if (header.indexOf("GET /1/on") >= 0) {
+                Serial.println("led 1 on");
+                etat_led1 = "on";
+                digitalWrite(led_box1, HIGH);
+              } else if (header.indexOf("GET /1/off") >= 0) {
+                Serial.println("led 1 off");
+                etat_led1 = "off";
+                digitalWrite(led_box1, LOW);
+              } else if (header.indexOf("GET /2/on") >= 0) {
+                Serial.println("led 2 on");
+                etat_led2 = "on";
+                digitalWrite(led_box2, HIGH);
+              } else if (header.indexOf("GET /2/off") >= 0) {
+                Serial.println("led 2 off");
+                etat_led2 = "off";
+                digitalWrite(led_box2, LOW);
               }
-            } else if (header.indexOf("GET /p1/close") >= 0) {
-                Serial.println("porte 1 close");
-                etat_porte1 = "close";
-                for (pos = 90; pos >= 0; pos -= 1) { // on tourne jusqu'a 0 degrés 
-                  servo.write(pos);
-                  delay(10);
-                }
-            } else if (header.indexOf("GET /p2/open") >= 0) {
-                Serial.println("porte 2 open");
-                etat_porte2 = "open";
+              
+              // Ouvrir et fermer porte
+              if (header.indexOf("GET /p1/open") >= 0) {
+                Serial.println("porte 1 open");
+                etat_porte1 = "open";
                 for (pos = 0; pos <= 90; pos += 1) { // on tourne jusqu'a 90 degrés 
                   servo.write(pos);
                   delay(10);
                 }
-            } else if (header.indexOf("GET /p2/close") >= 0) {
-                Serial.println("porte 2 close");
-                etat_porte2 = "close";
-                for (pos = 90; pos >= 0; pos -= 1) { // on tourne jusqu'a 0 degrés 
-                  servo.write(pos);
-                  delay(10);
-                }
+              } else if (header.indexOf("GET /p1/close") >= 0) {
+                  Serial.println("porte 1 close");
+                  etat_porte1 = "close";
+                  for (pos = 90; pos >= 0; pos -= 1) { // on tourne jusqu'a 0 degrés 
+                    servo.write(pos);
+                    delay(10);
+                  }
+              } else if (header.indexOf("GET /p2/open") >= 0) {
+                  Serial.println("porte 2 open");
+                  etat_porte2 = "open";
+                  for (pos = 0; pos <= 90; pos += 1) { // on tourne jusqu'a 90 degrés 
+                    servo.write(pos);
+                    delay(10);
+                  }
+              } else if (header.indexOf("GET /p2/close") >= 0) {
+                  Serial.println("porte 2 close");
+                  etat_porte2 = "close";
+                  for (pos = 90; pos >= 0; pos -= 1) { // on tourne jusqu'a 0 degrés 
+                    servo.write(pos);
+                    delay(10);
+                  }
+              }
+              
+              // Desactiver alarme
+              if (header.indexOf("GET /alarme/desactiver") >= 0) {
+                Serial.println("alarme desactiver");
+                etat_alarme = "desactiver";
+                digitalWrite(buzzer, LOW);
+              } else {
+                Serial.println("alarme activer");
+                etat_alarme = "eteint";
+                tone(buzzer, 440);
+              }
+            
+              // Affichage de la page html
+              client.println("<!DOCTYPE html><html>");
+              client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+              client.println("<link rel=\"icon\" href=\"data:,\">");
+              client.println("<script src=\"https://kit.fontawesome.com/a076d05399.js\"></script>");
+              client.println("<script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\"></script>");
+              client.println("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js\" integrity=\"sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4\"></script>");
+              client.println("<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js\" integrity=\"sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1\"></script>");
+              
+              // CSS
+              client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+              client.println(".button2 {background-color: #555555;}</style></head>");
+              
+              client.println("<body><h4>Gestion des fonctionalités</h4>");
+              
+              client.println("<div  class=\"row justify-content-center no-gutters outer-div\">");
+              
+                // On vérifie l'état des leds 
+                client.println("<div  class=\"col-xs-6 col-sm-6 col-md-6 col-lg-6 justify-content-center inner-div\">");
+                  client.println("<p>Etat Led 1 : " + etat_led1 + "</p>");      
+                  if (etat_led1=="off") {
+                    client.println("<p><a href=\"/1/on\"><button class=\"button\">ON</button></a></p>");
+                  } else {
+                    client.println("<p><a href=\"/1/off\"><button type=\"button\" class=\"btn btn-info col-xs-6 col-sm-6 col-md-6 col-lg-6 button2\">OFF</button></a></p>");
+                  } 
+                client.println("</div>");
+                client.println("<div  class=\"col-xs-6 col-sm-6 col-md-6 col-lg-6 inner-div\">");
+                  client.println("<p>Etat Led 2 : " + etat_led2 + "</p>");      
+                  if (etat_led2=="off") {
+                    client.println("<p><a href=\"/2/on\"><button type=\"button\" class=\"btn btn-info col-xs-6 col-sm-6 col-md-6 col-lg-6\">ON</button></a></p>");
+                  } else {
+                    client.println("<p><a href=\"/2/off\"><button type=\"button\" class=\"btn btn-info col-xs-6 col-sm-6 col-md-6 col-lg-6 button2\">OFF</button></a></p>");
+                  } 
+                client.println("</div>");
+              
+                // On vérifie l'état des portes 
+                client.println("<div  class=\"col-xs-6 col-sm-6 col-md-6 col-lg-6 justify-content-center inner-div\">");
+                  client.println("<p>Etat porte 1 : " + etat_porte1 + "</p>");      
+                  if (etat_porte1=="close") {
+                    client.println("<p><a href=\"/p1/open\"><button class=\"button\">OPEN</button></a></p>");
+                  } else {
+                    client.println("<p><a href=\"/p1/close\"><button type=\"button\" class=\"btn btn-info col-xs-6 col-sm-6 col-md-6 col-lg-6 button2\">CLOSE</button></a></p>");
+                  } 
+                client.println("</div>");
+                
+                client.println("<div  class=\"col-xs-6 col-sm-6 col-md-6 col-lg-6 justify-content-center inner-div\">");
+                  client.println("<p>Etat porte 2 : " + etat_porte2 + "</p>");      
+                  if (etat_porte2=="close") {
+                    client.println("<p><a href=\"/p2/open\"><button class=\"button\">OPEN</button></a></p>");
+                  } else {
+                    client.println("<p><a href=\"/p2/close\"><button type=\"button\" class=\"btn btn-info col-xs-6 col-sm-6 col-md-6 col-lg-6 button2\">CLOSE</button></a></p>");
+                  } 
+                client.println("</div>");
+                
+                // Desactivé alarme
+                client.println("<div  class=\"col-xs-6 col-sm-6 col-md-6 col-lg-6 justify-content-center inner-div\">");
+                  client.println("<p>Etat de l'alarme: " + etat_alarme + "</p>");      
+                  if (etat_alarme=="desactiver") {
+                    client.println("<p><a href=\"/alarme/desactiver\"><button class=\"button\">Vous avez desactiver l'alarme</button></a></p>");
+                  } else {
+                    client.println("<p><a href=\"/alarme/eteint\"><button type=\"button\" class=\"btn btn-info col-xs-6 col-sm-6 col-md-6 col-lg-6 button2\">Veuillez desactiver l'alarme</button></a></p>");
+                  } 
+                client.println("</div>");
+                
+                
+              client.println("</div>");
+              
+              client.println("</body></html>");
+              
+              client.println();
+              
+              break;
+            } else {
+              currentLine = "";
             }
-          
-          
-            // Affichage de la page html
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            client.println("<script src=\"https://kit.fontawesome.com/a076d05399.js\"></script>");
-            client.println("<script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\"></script>");
-            client.println("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js\" integrity=\"sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4\"></script>");
-            client.println("<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js\" integrity=\"sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1\"></script>");
-            
-            // CSS
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button2 {background-color: #555555;}</style></head>");
-            
-            client.println("<body><h4>Gestion des fonctionalités</h4>");
-            
-            client.println("<div  class=\"row justify-content-center no-gutters outer-div\">");
-            
-              // On vérifie l'état des leds 
-              client.println("<div  class=\"col-xs-6 col-sm-6 col-md-6 col-lg-6 justify-content-center inner-div\">");
-                client.println("<p>Etat Led 1 : " + etat_led1 + "</p>");      
-                if (etat_led1=="off") {
-                  client.println("<p><a href=\"/1/on\"><button class=\"button\">ON</button></a></p>");
-                } else {
-                  client.println("<p><a href=\"/1/off\"><button type=\"button\" class=\"btn btn-info col-xs-6 col-sm-6 col-md-6 col-lg-6 button2\">OFF</button></a></p>");
-                } 
-              client.println("</div>");
-              client.println("<div  class=\"col-xs-6 col-sm-6 col-md-6 col-lg-6 inner-div\">");
-                client.println("<p>Etat Led 2 : " + etat_led2 + "</p>");      
-                if (etat_led2=="off") {
-                  client.println("<p><a href=\"/2/on\"><button type=\"button\" class=\"btn btn-info col-xs-6 col-sm-6 col-md-6 col-lg-6\">ON</button></a></p>");
-                } else {
-                  client.println("<p><a href=\"/2/off\"><button type=\"button\" class=\"btn btn-info col-xs-6 col-sm-6 col-md-6 col-lg-6 button2\">OFF</button></a></p>");
-                } 
-              client.println("</div>");
-            
-            // On vérifie l'état des portes 
-              client.println("<div  class=\"col-xs-6 col-sm-6 col-md-6 col-lg-6 justify-content-center inner-div\">");
-                client.println("<p>Etat porte 1 : " + etat_porte1 + "</p>");      
-                if (etat_porte1=="close") {
-                  client.println("<p><a href=\"/p1/open\"><button class=\"button\">OPEN</button></a></p>");
-                } else {
-                  client.println("<p><a href=\"/p1/close\"><button type=\"button\" class=\"btn btn-info col-xs-6 col-sm-6 col-md-6 col-lg-6 button2\">CLOSE</button></a></p>");
-                } 
-              client.println("</div>");
-              
-              client.println("<div  class=\"col-xs-6 col-sm-6 col-md-6 col-lg-6 justify-content-center inner-div\">");
-                client.println("<p>Etat porte 2 : " + etat_porte2 + "</p>");      
-                if (etat_porte2=="close") {
-                  client.println("<p><a href=\"/p2/open\"><button class=\"button\">OPEN</button></a></p>");
-                } else {
-                  client.println("<p><a href=\"/p2/close\"><button type=\"button\" class=\"btn btn-info col-xs-6 col-sm-6 col-md-6 col-lg-6 button2\">CLOSE</button></a></p>");
-                } 
-              client.println("</div>");
-              
-            client.println("</div>");
-            
-            client.println("</body></html>");
-            
-            client.println();
-            
-            break;
-          } else {
-            currentLine = "";
+          } else if (c != '\r') {  // on vérifie si il n'y est pas de retour a la ligne
+            currentLine += c;
           }
-        } else if (c != '\r') {  // on vérifie si il n'y est pas de retour a la ligne
-          currentLine += c;
         }
       }
-    }
-    // On met le header a vide
-    header = "";
-  
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
+      // On met le header a vide
+      header = "";
     
+      client.stop();
+      Serial.println("Client disconnected.");
+      Serial.println("");
+    } else { // detection d'une présence mais pas de connection on fait sonner l'alarme
+        tone(buzzer, 440);
+        delay(1000);
+      }
+  } else { // Pas de présence on allume pas les leds
+    digitalWrite(led_presence1, LOW);
+    digitalWrite(led_presence2, LOW);
+    delay(100);
+  }
 }
